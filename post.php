655d575704bc138c20cfdb64a5f8caa7ae0778fb
nine009yu <152370454+nine009yu@users.noreply.github.com>
2024-01-03 16:01:11 +0700

    work4
    work4

---
+++ post.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1 style="text-align: center;">Webboard KakKak</h1>
+    <hr><br>
+    <div style="text-align: center;">
+    <?php
+        echo "ต้องการดูกระทู้หมายเลข ". $_GET['id'] 
+    ?><br><br>
+   <table style="border: 2px solid black; width: 40%;" align="center">
+        <tr><td colspan="2" style="background-color:#6CD2FE ;" >แสดงความคิดเห็น</td></tr>
+        <tr><td><textarea textarea name="message" rows="10" cols="60"></textarea></td></tr>
+        <tr><td colspan="2" align="center"><input type="submit" value="ส่งข้อความ"></td></tr>
+    </table><br>
+    <a href="index.html">กลับไปหน้าหลัก</a> 
+</div>
+</body>
+</html>
