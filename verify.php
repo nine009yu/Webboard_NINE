71e2f4e434bb7e070c4620d78c804a2d0633fb21
nine009yu <152370454+nine009yu@users.noreply.github.com>
2024-02-21 15:41:46 +0700

    Update webboard work7
    Update webboard work7

--- verify.php
+++ verify.php
@@ -14,10 +14,8 @@ if(isset($_SESSION['id'])){
     
     
 </head>
-<body>
-<h1 style="text-align: center;">Webboard KakKak</h1>
-    <hr><br>   
-    <div style="text-align: center;">
+<body>   
+    
     <?php   
     $Login = $_POST['login']; 
     $Password = $_POST['pwd'];
@@ -26,24 +24,27 @@ if(isset($_SESSION['id'])){
         $_SESSION["username"] = "admin";
         $_SESSION["role"] = "a";
         $_SESSION["id"] = session_id();
-        echo "ยินดีต้อนรับ ADMIN";
+        header("location:index.php");
+        die();
+        //echo "ยินดีต้อนรับ ADMIN";
     } 
     elseif(($Login =="member")&&($Password=="mem1234")){
         $_SESSION["username"] = "member";
         $_SESSION["role"] = "m";
         $_SESSION["id"] = session_id();
-        echo "ยินดีต้อนรับ MEMBER";
+        header("location:index.php");
+        die();
+       // echo "ยินดีต้อนรับ MEMBER";
     }
     else{
-        echo "ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง";
-    }
-    
-
-    
+        $_SESSION["error"]='error';
+        header("location:login.php");
+        die();
+       // echo "ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง";
+    } 
     ?>
     <br>
     <a href="index.php">กลับไปยังหน้าหลัก</a> 
-    </div>
 </body>
 </html>
 
