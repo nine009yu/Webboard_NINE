9b0c153383e0ca9988b81f60308af63e533bb8c2
nine009yu <152370454+nine009yu@users.noreply.github.com>
2024-03-13 16:42:37 +0700

    work 10 update
    work 10 update javascript

--- register.php
+++ register.php
@@ -14,8 +14,19 @@ if(isset($_SESSION['id'])){
     <script src="bootstrap-5.3.2-dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>register</title>
+   
 </head>
 <body>
+<script>
+        function OnBlurPwd(){
+            let pass=document.getElementById("pass");
+            let pass2=document.getElementById("pass2");
+            if(pass.value!==pass2.value) {
+                alert("รหัสผ่านทั้งสองช่องไม่ตงกัน");
+            pass2.value="";
+            }
+        }
+    </script>
     <div class="container-lg">
         <h1 style="text-align: center;" class="mt-3">Webboard KakKak</h1>
         <?php include "nav.php"?>
@@ -48,6 +59,13 @@ if(isset($_SESSION['id'])){
                                      <input id="pass" type="password" name="pwd" class="form-control" required>
                                 </div>
                             </div>
+                            <div class="row mt-3">
+                                <label for="pass2" class="col-lg-3 col-form-label">ใส่รหัสผ่านซ้ำ:</label>
+                                <div class="col-lg-9">
+                                     <input id="pass2" type="password" name="pwd2" onblur="OnBlurPwd()" class="form-control" required>
+                                </div>
+                            </div>
+                            
                             <div class="row mt-3">
                                 <label for="name" class="col-lg-3 col-form-label">ชื่อ-นามสกุล:</label>
                                 <div class="col-lg-9">
